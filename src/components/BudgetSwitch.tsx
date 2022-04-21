8ef198b418e9c1ed59f528fbbc0ab436f5957fef
Fynn Heintz <fynn.heintz@gmail.com>
2022-04-21 22:31:08 +0200

    get budgets from API

--- src/components/BudgetSwitch.tsx
+++ src/components/BudgetSwitch.tsx
@@ -1,7 +1,9 @@
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import { Translation, Budget } from '../types'
 import cookie from '../lib/cookie'
+import getBudgets from '../lib/getBudgets'
 
 type BudgetSwitchProps = {
   setBudgetId: (id: string) => void
@@ -10,27 +12,43 @@ type BudgetSwitchProps = {
 const BudgetSwitch = (props: BudgetSwitchProps) => {
   const { t }: Translation = useTranslation()
 
+  const [isLoading, setIsLoading] = useState(true)
+  const [budgets, setBudgets] = useState<Budget[]>([])
+
+  useEffect(() => {
+    loadBudgets()
+  }, [])
+
+  const loadBudgets = () => {
+    getBudgets().then(data => {
+      setBudgets(data)
+      setIsLoading(false)
+    })
+  }
+
   const setBudgetId = (id: string) => {
     cookie.set('budgetId', id)
     props.setBudgetId(id)
   }
 
-  const budgets: Budget[] = []
-
   return (
     <>
-      {budgets.map(budget => (
-        <div key={budget.id}>
-          <Link
-            to="/"
-            onClick={() => {
-              setBudgetId(budget.id.toString())
-            }}
-          >
-            {budget.name}
-          </Link>
-        </div>
-      ))}
+      {isLoading ? (
+        <div>loading...</div>
+      ) : (
+        budgets.map(budget => (
+          <div key={budget.id}>
+            <Link
+              to="/"
+              onClick={() => {
+                setBudgetId(budget.id.toString())
+              }}
+            >
+              {budget.name || `Budget ${budget.id}`}
+            </Link>
+          </div>
+        ))
+      )}
     </>
   )
 }
