25c48696bf80574c6eb021735a632f4cbcbd6e71
Fynn Heintz <fynn.heintz@gmail.com>
2022-05-04 10:40:43 +0200

    create new budgets

--- src/components/BudgetSwitch.tsx
+++ src/components/BudgetSwitch.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { Translation, Budget } from '../types'
 import { PencilIcon } from '@heroicons/react/solid'
+import { PlusCircleIcon } from '@heroicons/react/outline'
 import cookie from '../lib/cookie'
 import { getBudgets } from '../lib/api/budgets'
 import { formatMoney } from '../lib/format'
@@ -40,33 +41,40 @@ const BudgetSwitch = (props: BudgetSwitchProps) => {
       {isLoading ? (
         <LoadingSpinner />
       ) : (
-        <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
-          {budgets.map(budget => (
-            <li
-              key={budget.id}
-              className="box col-span-1 flex hover:bg-gray-200 p-4 hover:-translate-y-0.5 hover:shadow-md cursor-pointer"
-            >
-              <Link
-                to="/"
-                className="w-full text-center"
-                onClick={() => {
-                  setBudgetId(budget.id.toString())
-                }}
+        <>
+          <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
+            {budgets.map(budget => (
+              <li
+                key={budget.id}
+                className="box col-span-1 flex hover:bg-gray-200 p-4 hover:-translate-y-0.5 hover:shadow-md cursor-pointer"
               >
-                <h3>{budget.name || `${t('budgets.budget')} ${budget.id}`}</h3>
-                {budget.note ? (
-                  <p className="text-sm text-gray-500">{budget.note}</p>
-                ) : null}
-                <div className="text-lime-600 mt-2 text-lg">
-                  <strong>{formatMoney(1337, budget.currency)}</strong>
-                </div>
-              </Link>
-              <Link to={`/budgets/${budget.id}`} title={t('edit')}>
-                <PencilIcon className="icon" />
-              </Link>
-            </li>
-          ))}
-        </ul>
+                <Link
+                  to="/"
+                  className="w-full text-center"
+                  onClick={() => {
+                    setBudgetId(budget.id.toString())
+                  }}
+                >
+                  <h3>
+                    {budget.name || `${t('budgets.budget')} ${budget.id}`}
+                  </h3>
+                  {budget.note ? (
+                    <p className="text-sm text-gray-500">{budget.note}</p>
+                  ) : null}
+                  <div className="text-lime-600 mt-2 text-lg">
+                    <strong>{formatMoney(1337, budget.currency)}</strong>
+                  </div>
+                </Link>
+                <Link to={`/budgets/${budget.id}`} title={t('edit')}>
+                  <PencilIcon className="icon" />
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <Link to="/budgets/new" title={t('bugets.create')}>
+            <PlusCircleIcon className="icon icon-lg mx-auto mt-4" />
+          </Link>
+        </>
       )}
     </>
   )
