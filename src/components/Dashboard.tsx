44c4f1ba227afc54280697094b24eeaa0e499e04
Fynn Heintz <fynn.heintz@gmail.com>
2022-05-16 22:44:06 +0200

    clean up TODOs

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -6,10 +6,7 @@ type DashboardProps = { budget: Budget }
 const Home = ({ budget }: DashboardProps) => {
   return (
     <>
-      <div className="header">
-        <h1>{safeName(budget, 'budget')}</h1>
-        <div className="header--action">TODO</div>
-      </div>
+      <h1 className="header">{safeName(budget, 'budget')}</h1>
       <p>There should be some content here</p>
     </>
   )
