312534cbdf71d0f5a24a00b3615cd9187732f168
Fynn Heintz <fynn.heintz@gmail.com>
2022-05-20 23:48:05 +0200

    refactor: style sheets

--- src/components/AccountForm.tsx
+++ src/components/AccountForm.tsx
@@ -125,11 +125,8 @@ const AccountForm = ({ budget, type }: Props) => {
               </div>
             ) : null}
 
-            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-              <label
-                htmlFor="note"
-                className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-              >
+            <div className="form-field--wrapper">
+              <label htmlFor="note" className="form-field--label">
                 {t('accounts.note')}
               </label>
               <div className="mt-1 sm:mt-0 sm:col-span-2">
