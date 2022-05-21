98581fb0c5a003c78600582f55dbbbdd5be60878
Fynn Heintz <fynn.heintz@gmail.com>
2022-05-21 11:08:45 +0200

    fix dropdown styling

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -55,7 +55,7 @@ const Autocomplete = <T,>({
           </div>
 
           {filteredItems.length > 0 && (
-            <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Combobox.Options className="absolute z-10 mt-1 max-h-60 max-w-lg w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
               {filteredItems.map(item => (
                 <Combobox.Option
                   key={itemId(item)}
