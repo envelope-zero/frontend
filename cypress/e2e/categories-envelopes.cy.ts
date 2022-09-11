fbfc152db0cb645afbcfc22dcbb86db8be7c3f87
Fynn Heintz <fynn.heintz@gmail.com>
2022-09-11 14:02:29 +0200

    test: grouping multiple envelopes into different categories

--- cypress/e2e/categories-envelopes.cy.ts
+++ cypress/e2e/categories-envelopes.cy.ts
@@ -11,27 +11,43 @@ describe('Envelope: Creation', () => {
   it('can create a category for a budget', () => {
     cy.contains('Envelopes').click()
 
+    // Grocery Envelope (category Daily Spending)
     cy.getByTitle('Create Envelope').click()
-
     cy.getInputFor('Name').type('Grocery Envelope')
-
     cy.getInputFor('Category').type('Daily Spending')
     cy.contains('Create "Daily Spending"').click()
     cy.getInputFor('Note').type(
       'Groceries.{enter}{enter}Usually bought at a supermarket.'
     )
-
     cy.clickAndWait('Save')
-    cy.get('h1').contains('Envelopes')
+
+    // Restaurant Envelope (category Daily Spending)
+    cy.getByTitle('Create Envelope').click()
+    cy.getInputFor('Name').type('Restaurants')
+    cy.getInputFor('Category').type('Dail')
+    cy.contains('Daily Spending').click()
+    cy.clickAndWait('Save')
+
+    // Rent Envelope (category Running Costs)
+    cy.getByTitle('Create Envelope').click()
+    cy.getInputFor('Name').type('Rent')
+    cy.getInputFor('Category').type('Running Costs')
+    cy.contains('Create "Running Costs"').click()
+    cy.clickAndWait('Save')
 
     // After saving, the Accordion for the new category should be
     // expanded.
     // We check this, click it to collapse and verify that it collapsed
     //
     // Get the accordion first to ensure it has been loaded
-    cy.get('.grow > span').contains('Daily Spending').as('category')
+    cy.get('.grow > span').contains('Daily Spending').as('dailySpending')
+    cy.get('.grow > span').contains('Running Costs').as('runningCosts')
+
     cy.contains('Grocery Envelope')
-    cy.get('@category').click()
+    cy.get('@dailySpending').click()
     cy.contains('Grocery Envelope').should('not.exist')
+
+    // envelopes from other categories are still visible
+    cy.contains('Rent')
   })
 })
