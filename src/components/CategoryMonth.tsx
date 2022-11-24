65c5b932454d5a3f38839dcb44006bc13ea5c6b9
Fynn Heintz <fynn.heintz@gmail.com>
2022-11-24 20:48:34 +0100

    make categories in dashboard collapsible

--- src/components/CategoryMonth.tsx
+++ src/components/CategoryMonth.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { CategoryMonth as CategoryMonthType, Budget, UUID } from '../types'
 import EnvelopeMonth from './EnvelopeMonth'
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid'
 
 type props = {
   category: CategoryMonthType
@@ -18,29 +20,53 @@ const CategoryMonth = ({
   reloadBudgetMonth,
   setError,
 }: props) => {
+  const [showEnvelopes, setShowEnvelopes] = useState(true)
+
   return (
     <>
-      <tr className="border-t border-gray-200">
+      <tr
+        className="border-t border-gray-200 bg-gray-50 cursor-pointer"
+        onClick={() => {
+          if (
+            category.envelopes.some(envelope => envelope.id === editingEnvelope)
+          ) {
+            editEnvelope(undefined)
+          }
+
+          setShowEnvelopes(!showEnvelopes)
+        }}
+      >
         <th
           colSpan={3}
           scope="colgroup"
-          className="bg-gray-50 px-4 py-2 text-left text-sm font-bold text-gray-900 sm:px-6 text-ellipsis"
+          className="px-4 py-2 text-left text-sm font-bold text-gray-900 sm:px-6 text-ellipsis"
         >
-          {category.name}
+          <span className="flex items-center">
+            <button>
+              {showEnvelopes ? (
+                <ChevronUpIcon className="icon" />
+              ) : (
+                <ChevronDownIcon className="icon" />
+              )}
+            </button>
+            {category.name}
+          </span>
         </th>
       </tr>
-      {category.envelopes.map((envelope, i) => (
-        <EnvelopeMonth
-          key={envelope.id}
-          envelope={envelope}
-          i={i}
-          budget={budget}
-          editingEnvelope={editingEnvelope}
-          editEnvelope={editEnvelope}
-          reloadBudgetMonth={reloadBudgetMonth}
-          setError={setError}
-        ></EnvelopeMonth>
-      ))}
+      {showEnvelopes
+        ? category.envelopes.map((envelope, i) => (
+            <EnvelopeMonth
+              key={envelope.id}
+              envelope={envelope}
+              i={i}
+              budget={budget}
+              editingEnvelope={editingEnvelope}
+              editEnvelope={editEnvelope}
+              reloadBudgetMonth={reloadBudgetMonth}
+              setError={setError}
+            ></EnvelopeMonth>
+          ))
+        : null}
     </>
   )
 }
