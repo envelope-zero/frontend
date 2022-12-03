210c5369c675fbaf9b520e6669ea16d624e7f96f
Morre <morre@mor.re>
2022-12-03 15:39:09 +0100

    feat: add spent for whole month

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -211,8 +211,16 @@ const Dashboard = ({ budget }: DashboardProps) => {
                           )}
                         </td>
                         <td
-                          className={`hidden md:table-cell whitespace-nowrap px-3 pb-3 text-sm font-semibold text-right`}
-                        ></td>
+                          className={`hidden md:table-cell whitespace-nowrap px-3 pb-3 text-sm font-semibold text-right ${
+                            budgetMonth.spent < 0 ? 'positive' : 'text-gray-500'
+                          }`}
+                        >
+                          {formatMoney(
+                            budgetMonth.spent,
+                            budget.currency,
+                            'auto'
+                          )}
+                        </td>
                         <td
                           className={`whitespace-nowrap pl-3 pr-4 sm:pr-6 pb-3 text-sm font-semibold text-right ${
                             budgetMonth.balance < 0
