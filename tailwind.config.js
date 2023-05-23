bdb6ec9e78fec37b19b5ff96315601cb708b58a6
Fynn Heintz <fynn.heintz@gmail.com>
2023-05-23 21:28:05 +0200

    chore: add flowbite for tooltips

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
   darkMode: 'class',
 }
