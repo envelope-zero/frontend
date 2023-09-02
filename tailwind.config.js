ae5da7e493fb6c273867af007242b8281c6456f3
morre <morre@mor.re>
2023-09-02 18:16:33 +0200

    fix: CSS not rendering correctly in production build (#1086)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
