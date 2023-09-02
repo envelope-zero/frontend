075da340d9c8fc6ef0cf8c0a7abd1653b758397a
Morre <morre@mor.re>
2023-09-02 12:32:51 +0200

    chore: run frontend natively for development and testing
    This eliminates cross-platform issues, especially when developing on macOS.
    The cost is mainly that we now need to execute one command more than before,
    but maintenance is much easier.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/e2e.ts',
   },
 })
