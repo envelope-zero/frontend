ca345bb8add119384c2dbf5c9c9712ee85978e09
Morre <morre@mor.re>
2023-09-21 19:58:28 +0200

    chore: remove vtie-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
-import svgrPlugin from 'vite-plugin-svgr'
 import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), eslint(), viteTsconfigPaths()],
   resolve: {
     alias: {
       stream: 'stream-browserify', // Needed so that charset detection for ynap-parsers works
