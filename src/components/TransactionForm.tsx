e182c55ba8eec3c8dda58cff6930ee017b15164c
morre <morre@mor.re>
2024-01-12 12:06:10 +0100

    Update src/components/TransactionForm.tsx
    Co-authored-by: Fynn Heintz <fynn.heintz@gmail.com>

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -328,6 +328,8 @@ const TransactionForm = ({ budget, setNotification }: Props) => {
                         }
                       })
                     )
+                  } else if (!account.external) {
+                    setRecentEnvelopes([])
                   }
 
                   Promise.all(envelopePromises).then(() =>
