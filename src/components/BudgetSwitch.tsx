b3bd84fbf2bf90929e9166c0006ea89b8562b43c
Fynn Heintz <fynn.heintz@gmail.com>
2024-01-27 10:30:16 +0100

    feat: improve list views

--- src/components/BudgetSwitch.tsx
+++ src/components/BudgetSwitch.tsx
@@ -2,9 +2,13 @@ import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { Translation, Budget } from '../types'
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
 import { PencilIcon } from '@heroicons/react/24/solid'
-import { PlusCircleIcon, PlusIcon } from '@heroicons/react/24/outline'
+import {
+  ArrowsRightLeftIcon,
+  EyeIcon,
+  PlusCircleIcon,
+  PlusIcon,
+} from '@heroicons/react/24/outline'
 import connectBudgetApi from '../lib/api/budgets'
 import LoadingSpinner from './LoadingSpinner'
 import { safeName } from '../lib/name-helper'
@@ -13,6 +17,7 @@ import FlyoutMenu from './FlyoutMenu'
 
 type BudgetSwitchProps = {
   selectBudget: (budget?: Budget) => void
+  currentBudget?: Budget
 }
 
 const BudgetSwitch = (props: BudgetSwitchProps) => {
@@ -66,54 +71,76 @@ const BudgetSwitch = (props: BudgetSwitchProps) => {
       ) : (
         <div className="mt-3">
           {budgets.length ? (
-            <ul className="grid grid-cols-1 gap-5 sm:gap-6">
-              {budgets.map(budget => (
-                <li
-                  key={budget.id}
-                  className="card col-span-1 relative hover:bg-gray-50 dark:hover:bg-slate-700 p-4 md:hover:-translate-y-0.5 hover:shadow-md cursor-pointer"
-                >
-                  <Link
-                    to="/"
-                    className="w-full text-center"
-                    onClick={() => {
-                      props.selectBudget(budget)
-                    }}
+            <>
+              <ul className="grid grid-cols-1 gap-10 lg:gap-6 lg:grid-cols-2">
+                {budgets.map(budget => (
+                  <li
+                    key={budget.id}
+                    className={`card p-0 col-span-1 divide-y divide-gray-200 dark:divide-gray-600 flex flex-col justify-between ${
+                      budget.id === props.currentBudget?.id
+                        ? 'border-2 border-red-800 dark:border-red-600'
+                        : ''
+                    }`}
                   >
-                    <div className="flex justify-center">
-                      <h3
-                        className={
-                          typeof budget.name === 'undefined' ? 'italic' : ''
-                        }
-                      >
-                        {safeName(budget, 'budget')}
-                      </h3>
+                    <div className="flex w-full items-center justify-between space-x-6 p-6">
+                      <div className="flex-1 truncate">
+                        <div className="flex items-center space-x-3">
+                          <h3
+                            className={`truncate text-sm ${
+                              budget.name === '' ? 'italic' : ''
+                            }`}
+                          >
+                            {safeName(budget, 'budget')}
+                          </h3>
+                        </div>
+                        {budget.note && (
+                          <p className="mt-1 truncate text-sm text-gray-500 dark:text-gray-400">
+                            {budget.note}
+                          </p>
+                        )}
+                      </div>
                       <Link
+                        className="flex-shrink-0"
                         to={`/budgets/${budget.id}`}
                         title={t('edit')}
-                        className="pl-2"
                       >
                         <PencilIcon className="icon-red icon-sm" />
                       </Link>
                     </div>
-                    {budget.note ? (
-                      <p className="text-sm text-gray-500 dark:text-gray-400 whitespace-pre-line">
-                        {budget.note}
-                      </p>
-                    ) : null}
-                    <div className="flex justify-end">
-                      <Link
-                        to="/transactions/new"
-                        onClick={() => props.selectBudget(budget)}
-                        className="text-sky-600 hover:text-sky-700 dark:text-sky-400"
-                      >
-                        {t('transactions.add')}
-                        <ChevronRightIcon className="inline h-6" />
-                      </Link>
+                    <div className="-mt-px flex divide-x divide-gray-200 dark:divide-gray-600">
+                      <div className="flex w-0 flex-1">
+                        <Link
+                          to="/"
+                          onClick={() => {
+                            props.selectBudget(budget)
+                          }}
+                          className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900 dark:text-gray-100"
+                        >
+                          <EyeIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                          {t('open')}
+                        </Link>
+                      </div>
+                      <div className="-ml-px flex w-0 flex-1">
+                        <Link
+                          to="/transactions/new"
+                          onClick={() => props.selectBudget(budget)}
+                          className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900 dark:text-gray-100"
+                        >
+                          <ArrowsRightLeftIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                          {t('transactions.add')}
+                        </Link>
+                      </div>
                     </div>
-                  </Link>
-                </li>
-              ))}
-            </ul>
+                  </li>
+                ))}
+              </ul>
+            </>
           ) : (
             <>
               <div>{t('budgets.emptyList')}</div>
