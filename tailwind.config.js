b5d6e91c39bf452c81e803232d33d651e2874754
Morre <morre@mor.re>
2025-04-04 10:57:51 +0200

    fixup! chore(deps): update dependency flowbite-react to v0.11.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
+const flowbiteReact = require('flowbite-react/plugin/tailwindcss')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    '.flowbite-react/class-list.json',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
+    flowbiteReact,
+  ],
   darkMode: 'class',
 }
